8ead305d67d00ab45785ba681cded2fe72bd1572
Mai Diep <diepmv@vccloud.vn>
2018-06-19 15:01:01 +0700

    add pagination class

--- api/config.py
+++ api/config.py
@@ -10,3 +10,6 @@ SQLALCHEMY_TRACK_MODIFICATIONS=True
 
 SQLALCHEMY_DATABASE_URI = "postgresql://{DB_USER}:{DB_PASS}@{DB_ADDR}/{DB_NAME}".format(DB_USER="diepmv", DB_PASS="maivandiep", DB_ADDR="127.0.0.1", DB_NAME="messages")
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+PAGINATION_PAGE_SIZE = 5
+PAGINATION_PAGE_ARGUMENT_NAME = 'page'
